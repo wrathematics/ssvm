9a66a8407bcd2aa0079fc6bdea883fe9b6974feb
wrathematics <wrathematics@gmail.com>
2016-10-08 12:14:45 -0400

    some R package boilerplate

---
+++ R/ssvm-pkg.r
@@ -0,0 +1,12 @@
+#' ssvm: Simple SVM
+#' 
+#' @description
+#' A simple linear SVM implementation.
+#' 
+#' @useDynLib ssvm
+#' 
+#' @docType package
+#' @name ssvm-package
+#' @author Drew Schmidt
+#' @keywords package
+NULL
