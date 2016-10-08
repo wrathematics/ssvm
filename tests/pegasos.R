c69f8fc5cc8476a4c177e76664dca20c2ff07405
wrathematics <wrathematics@gmail.com>
2016-10-08 14:11:36 -0400

    add a test

---
+++ tests/pegasos.R
@@ -0,0 +1,30 @@
+library(ssvm)
+
+y <- recode(iris$Species == "setosa")
+x <- as.matrix(iris[, -5])
+
+set.seed(1234)
+truth <-
+structure(c(-0.138687298992951, -0.468900779129492, 0.682738598681026, 
+0.338677882941678), .Names = c("Sepal.Length", "Sepal.Width", 
+"Petal.Length", "Petal.Width"))
+test <- pegasos(x, y, intercept=FALSE)$w
+stopifnot(all.equal(test, truth))
+
+
+truth <-
+structure(c(-0.0798928770978866, -0.124892553609644, -0.457169655873265, 
+0.681240396757289, 0.335477893119601), .Names = c("Intercept", 
+"Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"))
+test <- pegasos(x, y, intercept=TRUE)$w
+stopifnot(all.equal(test, truth))
+
+
+
+#library(rbenchmark)
+#m = 2000
+#n = 650
+#x = matrix(rnorm(m*n), m, n)
+#y = sample(c(-1, 1), m, replace=TRUE)
+#system.time(pegasos(x, y, intercept=T))
+## benchmark(pegasos(x, y, intercept=FALSE), replications=25)
