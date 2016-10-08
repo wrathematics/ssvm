0cb35898f18492c9055c062d5de5c2b8c4ffcc5b
wrathematics <wrathematics@gmail.com>
2016-10-08 14:06:26 -0400

    use ssvm.so, export native routines

--- R/ssvm-pkg.r
+++ R/ssvm-pkg.r
@@ -4,6 +4,9 @@
 #' A simple two-class linear SVM implementation using
 #' the Pegasos algorithm.
 #' 
+#' @useDynLib ssvm, R_svm_pegasos_fit, R_svm_pegasos_pred,
+#'   R_svm_recode_response
+#' 
 #' @docType package
 #' @name ssvm-package
 #' @author Drew Schmidt
